cb8f123159e97e6e2af51211eb377636e8c3806d
Ram Krishna Shukla <your-email@gmail.com>
2026-05-01 01:33:38 +0530

    fix duplicate export

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { createProject, getProjects } from "../controllers/projectController.js";
-import { authMiddleware } from "../middleware/authMiddleware.js";
+import authMiddleware from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
